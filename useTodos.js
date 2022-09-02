747d8cea1dae2bc8c6430d45986426d3be7342b0
Luis Farfan <luise.prado99@hotmail.com>
2022-09-01 20:40:13 -0600

    feat: hooks added

---
+++ useTodos.js
@@ -0,0 +1,51 @@
+import { useEffect, useReducer } from 'react'
+import { todoReducer } from '../10-useReducer/todoReducer'
+
+const init = () => {
+  return JSON.parse(localStorage.getItem('todos')) || []
+}
+
+export const useTodos = () => {
+  // El tercer argumento es para correr algo para iniciar el estado
+  const [todos, dispath] = useReducer(todoReducer, [], init)
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos))
+  }, [todos])
+
+  const handleNewTodo = (todo) => {
+    const action = {
+      type: 'Add',
+      payload: todo
+    }
+
+    dispath(action)
+  }
+
+  const handleDeleteTodo = (todoId) => {
+    const action = {
+      type: 'Delete',
+      payload: todoId
+    }
+
+    dispath(action)
+  }
+
+  const handleTodoDone = (id) => {
+    const action = {
+      type: 'Done',
+      payload: id
+    }
+
+    dispath(action)
+  }
+
+  return {
+    todos,
+    handleNewTodo,
+    handleDeleteTodo,
+    handleTodoDone,
+    todosCount: todos.length,
+    pendingTodosCount: todos.filter((todo) => todo.done).length
+  }
+}
